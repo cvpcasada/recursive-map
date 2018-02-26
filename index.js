292d22593c54e640b909b3d83f96c9536e3e394e
Cyrus Venn Casada <cvpcasada@outlook.com>
2018-02-26 15:39:49 +0800

    Fixes obj namespace issues, Added documentation

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 import curryN from "curry-n";
 
-export const mapTree = curryN(3, (fn, childProp, obj) => {
-  return fn(
-    obj[childProp]
-      ? Object.assign({}, obj, {
-          [childProp]: recursiveMap(fn, childProp, obj[childProp])
+/**
+ * @name mapTree
+ * @param {function} fn - Function that produces a new tree object
+ * @param {string} childkey - Reference key to the children nodes in the object
+ * @param {Object[]} arr - The Array of trees to be mapped
+ * @returns {Object} A new tree being the result of the callback function
+ */
+export const mapTree = curryN(3, (fn, childKey, currentValue) =>
+  fn(
+    Array.isArray(currentValue[childKey])
+      ? Object.assign({}, currentValue, {
+          [childKey]: recursiveMap(fn, childKey, currentValue[childKey])
         })
-      : obj
-  );
-});
+      : currentValue
+  )
+);
 
-export const recursiveMap = curryN(3, (fn, childProp, arr) =>
-  arr.map(obj => mapTree(fn, childProp, obj))
+/**
+ * @name recursiveMap
+ * @param {function} fn - Function that produces an element for the new Array of trees
+ * @param {string} childkey - Reference key to the children nodes in the object
+ * @param {Object[]} arr - The Array of trees to be mapped
+ * @returns {Object[]} A new array of trees with each tree being the result of the callback function
+ */
+export const recursiveMap = curryN(3, (fn, childKey, arr) =>
+  arr.map(mapTree(fn, childKey))
 );
